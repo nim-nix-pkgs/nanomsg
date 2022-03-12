fda5945a45b06488ad15304f23d89bddd8730653
hugosenari <hugosenari@gmail.com>
2022-03-12 04:05:41 -0300

    chore: re index nanomsg

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Wrapper for the nanomsg socket library that provides several common communication patterns'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nanomsg-master.flake = false;
+  inputs.src-nanomsg-master.owner = "def-";
+  inputs.src-nanomsg-master.ref   = "refs/heads/master";
+  inputs.src-nanomsg-master.repo  = "nim-nanomsg";
+  inputs.src-nanomsg-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nanomsg-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nanomsg-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
